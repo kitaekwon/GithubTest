cbbfb9e342bd5d4f3f08bdee3a379c111a864a38
Kitae Kwon <kitkwon@yahoo.com>
2023-09-25 00:44:52 -0700

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by admin on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
