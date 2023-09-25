6d688e9ccead12c8aaca6279a750d811ef002415
Kitae Kwon <kitkwon@yahoo.com>
2023-09-25 00:48:22 -0700

    change text content.

--- GithubTest/ContentView.swift
+++ GithubTest/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Body
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!\nThis is atest")
         }
         .padding()
     }
